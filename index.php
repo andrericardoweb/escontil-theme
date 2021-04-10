77f45d76976850118dcbf11cdb7c7a2e110feca4
Andre Ricardo <andrericardoweb@gmail.com>
2021-04-10 02:57:50 -0300

    Loop post index

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
-  <p>Página Index </p>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  
+<h1><?php the_title(); ?></h1>
+<?php the_content(); ?>
+
+<?php endwhile; else: ?>
+<p><?php _e('Desculpe, não há publicações'); ?></p>
+<?php endif ?>
 
 <?php get_footer(); ?>
