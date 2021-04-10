8ff3f466aca44fd7a46038cfbc5f2b171ebc006b
Andre Ricardo <andrericardoweb@gmail.com>
2021-04-10 01:13:33 -0300

    Create header e footer

---
+++ footer.php
@@ -0,0 +1,60 @@
+  <!-- Rodape 1 -->
+  <footer>
+    <div class="rodape1-bg">
+      <div class="container rodape1">
+        <div class="rodape-redes rodape1-item">
+          <div class="rodape-icon"><img src="images/icons/like.svg"></div>
+          <h3>Nossas Redes Sociais</h3>
+          <p>Acompanhe nossos trabalhos atraves de nossas redes sociais.</p>
+          <div class="rodape-redes-btn">
+            <a href="#">Instagram</a>
+            <a href="#">Facebook</a>
+          </div>
+        </div><!--rodape-redes-->
+        <div class="rodape-localizacao rodape1-item">
+          <div class="rodape-icon"><img src="images/icons/placeholder.svg"></div>
+          <h3>Nossa Localização</h3>
+          <p>Praça Senador Eneas Araújo, nº 24 B<br>Centro, Santana do Ipanema/AL<br>CEP: 57500-000</p>
+        </div>
+        <div class="rodape-contatos rodape1-item">
+          <div class="rodape-icon"><img src="images/icons/telephone.svg"></div>
+          <h3>Nossos Contatos</h3>
+          <p>(82) 3621-1108 | 98127-8588 | 98727-0837<br>contato@escontilcontabil.com.br</p>
+          <div class="diferenciais-btns">
+            <a class="btn-whatsapp-banner" href="#">Atendimento via WhatsApp</a>
+          </div>         
+        </div>
+      </div><!--container rodape1-->
+            
+    </div><!--rodape1-bg-->
+
+    <!-- RODAPE 2 - MENU RODAPE -->
+    <div class="container">
+      <div class="rodape2">
+        <ul class="menu-rodape">
+          <li><a href="#">Links Úteis</a></li>
+          <li><a href="#">Trabalhe Conosco</a></li>
+          <li><a href="#">Privacidade</a></li>
+          <li><a href="#">Cookies</a></li>
+        </ul>
+      </div>                
+    </div>
+
+    <!-- RODAPE 3 -->
+    <div class="rodape3-bg">
+      <div class="container">
+        <div class="rodape3">
+          <p><sup>©</sup> 2021. Alguns direitos reservados.</p>
+          <a href="#"><img src="images/andrericardoweb-black.png" alt="Desenvolvedor: andrericardoweb"></a>
+        </div>
+        
+      </div>
+    </div>
+  </footer>
+
+  <!-- Início Wordpress Footer -->
+  <?php wp_footer(); ?>
+  <!-- Final Wordpress Footer -->
+    
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <!-- Metas -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+  <!-- Fontes -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,400;0,700;1,400&family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap"
+    rel="stylesheet">
+
+  <!-- font awesome -->
+  <script src="https://kit.fontawesome.com/16bd46d00e.js" crossorigin="anonymous"></script>
+  
+  <!-- Styles -->
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+
+  <title>Escontil Contadores Associados</title>
+
+  <!-- Início Wordpress Header -->
+  <?php wp_head(); ?>
+  <!-- Final Wordpress Header -->
+</head>
+
+<body>
+  <!-- SUPERINFO  -->
+  <div class="superinfo-bg">
+    <div class="container superinfo">
+      <div class="superinfo-item">
+        <p>ATENDEMOS MACEIÓ E SERTÃO ALAGOANO</p>
+      </div>
+
+      <div class="superinfo-item superinfo-telefones">
+        <p>(82) 3621-1108 | 98127-8588 | 98727-0837</p>
+      </div> <!--superinfo-contatos-->  
+      
+      <div class="superinfo-item superinfo-socialmedia">
+        <ul class="socialmedia">
+          <li><a href="#"><img src="images/icons/whatsapp.svg" alt="Whatsapp"></a></li>
+          <li><a href="#"><img src="images/icons/instagram.svg" alt="Instagram"></a></li>
+          <li><a href="#"><img src="images/icons/facebook.svg" alt="Facebook"></a></li>
+        </ul><!--socialmedia-->
+      </div><!--superinfo-socialmedia-->  
+    </div> <!--container superinfo-->
+  </div><!-- superinfo-bg -->
+
+  <!--  MENU  -->
+  <header>
+    <section class="container container-menu">
+      <div class="logo">
+        <a href="index.html"><img src="images/escontil-logo.png" alt="Escontil Associados"></a>
+      </div><!--logo-->
+
+      <nav class="menu-topo">
+        <input type="checkbox" id="menu-toggler" />
+
+        <label class="menu-toggler-icon" for="menu-toggler">
+          <i class="fas fa-bars"></i>
+        </label>
+
+        <ul>
+          <li class="logo-menu-mobile"><img src="images/escontil-logo.png" alt="Escontil Associados"></li>
+          <li><a class="menu-ativo" href="index.html">Ínicio</a></li>
+          <li><a href="quem-somos.html">Quem Somos</a></li>
+          <li><a href="servicos.html">Serviços</a></li>
+          <li><a href="clientes.html">Clientes</a></li>
+          <li><a href="parceiros.html">Parceiros</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="contato.html">Contato</a></li>
+          <label class="menu-toggler-icon-close" for="menu-toggler">
+            <i class="fas fa-times"></i> Fechar
+          </label> 
+        </ul><!--menu-topo-->
+      </nav>
+      
+      <div class="whatsapp-top">
+        <a href="#" class="whatsapp-btn-top">
+          <img src="images/icons/whatsapp.svg" alt="Whatsapp">
+        </a>
+      </div> <!--whatsapp-top-->
+
+    </section><!--container-menu-->
+  </header>
