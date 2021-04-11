35da0e556f65653dd4fd51558a6201cfb8206c39
Andre Ricardo <andrericardoweb@gmail.com>
2021-04-11 18:57:41 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,4 +12,7 @@ remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action('admin_print_styles', 'print_emoji_styles');
 
+// Habilitar Menus
+add_theme_support('menus');
+
 ?>
