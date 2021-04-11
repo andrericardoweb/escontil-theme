e5128acdb11579c04461e7a7c93d03d0a364fee4
Andre Ricardo <andrericardoweb@gmail.com>
2021-04-11 01:49:23 -0300

    Update page-quem-somos.php
    Custom field

--- page-quem-somos.php
+++ page-quem-somos.php
@@ -11,29 +11,14 @@
   <!-- Quem Somos -->
   <section class="container quem-somos">
     <div class="flex-item-1 quem-somos-img">
-      <img src="images/quem-somos.jpg" alt="">
+      <img src="<?php the_field('quemsomos_img'); ?>" alt="">
     </div>
     <div class="flex-item-2 quem-somos-txt">
-      <p class="quem-somos-txt-destaque">Nossa empresa de serviços de contabilidade possui uma equipe de profissionais
-        qualificados nas áreas contábil, fiscal, trabalhista e de assessoria.</p>
+      <p class="quem-somos-txt-destaque"><?php the_field('quemsomos_txt_destaque'); ?></p>
 
-      <p>Atuamos no mercado auxiliando as empresas, quanto a sua constituição, administração, consultorias e quando
-        necessário, no encerramento das mesmas.</p>
+      <?php the_field('quemsomos_txt'); ?>
 
-      <p>O planejamento tributário faz parte de nosso currículo empresarial, analisamos a tributação que mais se adequa
-        a sua empresa.</p>
-
-      <p>O mundo econômico atual – tão complexo e instável – em que temos de atuar; e a rapidez das informações exigem
-        de nós, administradores, um esforço para nos adaptarmos a esse ritmo; e, principalmente, para obtermos nosso
-        objetivo maior, o sucesso.</p>
-
-      <p>Neste contexto, contamos com uma equipe capacitada, preocupada em buscar constantemente o que há de mais novo
-        no mercado e em desenvolver um trabalho sério, competente e ético. Objetivando agregar valores, aumentar o
-        capital intelectual de nossos clientes, fazendo com que se tornem organizações bem-sucedidas e preparadas para
-        prosseguir neste caminho de conquistas e vitórias.</p>
-
-      <p>Nosso intuito é estabelecer estratégias e operacionalizar um conjunto de ações para os nossos parceiros
-        (clientes), capaz de fazê-los sobreviver, crescer e se perpetuar no mercado.</p>
+      
     </div>
   </section>
 
@@ -43,39 +28,29 @@
       <div class="flex-item-1 missa-visao-valores-item">
         <div class="missao-item">
           <h3>Missão</h3>
-          <p>Atender as necessidades dos clientes com soluções contábeis de excelência e qualidade, utilizando-se de
-            ferramentas tecnológicas avançadas e princípios legais, que possam suprir as variadas demandas do mercado.
-          </p>
+          <p><?php the_field('quemsomos_missao'); ?></p>
         </div>
 
         <div class="visao-item">
           <h3>Visão</h3>
-          <p>Ser referência na oferta de Soluções Contábeis para empresas do Sertão Alagoano e Maceió até 2025.
-          </p>
+          <p><?php the_field('quemsomos_visao'); ?></p>
         </div>
-      </div>
+      </div><!--missa-visao-valores-item-->
 
       <div class="flex-item-2 missa-visao-valores-item">
         <div class="valores-item">
           <h3>Valores</h3>
           <ul>
-            <li><i class="fas fa-angle-right"></i> Prestar serviços com Ética e Profissionalismo;</li>
-            <li><i class="fas fa-angle-right"></i> Ser uma extensão dos empreendimentos, estabelecendo Parceria e
-              primando pelo desenvolvimento por meio da Empatia;</li>
-            <li><i class="fas fa-angle-right"></i> Inovação nos serviços ofertados;</li>
-            <li><i class="fas fa-angle-right"></i> Promover a Educação contínua visando uma gestão empresarial mais
-              assertiva;</li>
-            <li><i class="fas fa-angle-right"></i> Motivação e Qualificação dos colaboradores promovendo um clima
-              organizacional saudável;</li>
-            <li><i class="fas fa-angle-right"></i> Atuar com Integridade, Disciplina e Proatividade enaltecendo a
-              profissão contábil;</li>
-            <li><i class="fas fa-angle-right"></i> Agir com Responsabilidade e Sustentabilidade diante da sociedade.
-            </li>
+
+            <?php if(have_rows('item_valores')) : while(have_rows('item_valores')) : the_row() ?>            
+              <li><i class="fas fa-angle-right"></i> <?php the_sub_field('valores'); ?></li>            
+            <?php endwhile; else : endif; ?>
+
           </ul>
         </div>
-      </div>
-    </div>
-  </section>
+      </div><!--missa-visao-valores-item-->
+    </div><!--missa-visao-valores-->
+  </section><!--missa-visao-valores-bg-->
 
   <!-- CTA -->
   <?php include(TEMPLATEPATH . "/inc/cta-section.php"); ?>
@@ -86,23 +61,16 @@
 
     <div class="container diretores">
       <div class="diretores-item">
-        <img src="images/monica-messias-diretores.jpg" alt="MONICA MESSIAS DE OLIVEIRA">
-        <h3>Mônica Messias de Olivera</h3>
-        <p>Bacharel em Ciências Contábeis (UFAL), inscrita no CRC/AL e no Cadastro Nacional de Peritos Contábeis (CNPC).
-          MBA em Contabilidade e Direito Tributário (IPOG). Pós-graduanda MBA em Legislação Trabalhista e Direito
-          Previdenciário (BSSP). Life Coaching e Analista Comportamental (SLAC). Qualificação em Compliance
-          Contábil.<br><br>Atua como Contadora/Empresária Contábil desde 2014 e Perita Contábil desde 2017.</p>
+        <img src="<?php the_field('diteroria1_imagem'); ?>">
+        <h3><?php the_field('diteroria1_nome'); ?> </h3>
+        <p><?php the_field('diteroria1_descricao'); ?></p>
       </div>
       <!---diretores-item-->
 
       <div class="diretores-item">
-        <img src="images/douglas-andre.jpg" alt="DOUGLAS ANDRÉ FERREIRA MARSIGLIA">
-        <h3>Douglas André Ferreira Marsiglia</h3>
-        <p>Bacharel em Ciências Contábeis (UFAL), inscrito no CRC/AL. Licenciado em Pedagogia (UNEAL). Especialista em
-          e-Social (Nith Treinamentos). Qualificação em empreendedorismo pela Endeavor Brasil. Pós-graduando MBA em
-          Contabilidade, Gestão Tributária e Auditoria (Faculdade Estratego). Pós-graduando MBA em Consultoria e
-          Empreendedorismo Contábil (BSSP). Pós-graduando especialização em Pedagogia Empresarial e Educação Corporativa
-          (FAEL/Vanta Education).<br>Atua como Contador/Empresário Contábil desde 2018.</p>
+        <img src="<?php the_field('diteroria2_imagem'); ?>">
+        <h3><?php the_field('diteroria2_nome'); ?></h3>
+        <p><?php the_field('diteroria2_descricao'); ?></p>
       </div>
       <!---diretores-item-->
     </div>
