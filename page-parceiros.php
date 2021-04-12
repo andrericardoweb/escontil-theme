00e8c60f1670d59255971d61e0a58574ece5c6e2
Andre Ricardo <andrericardoweb@gmail.com>
2021-04-12 20:49:09 -0300

    Update page-parceiros.php
    Custom field - Wordpress. Parceiros em Destaque

--- page-parceiros.php
+++ page-parceiros.php
@@ -29,60 +29,27 @@
     </div><!--servicos-->
   </section><!--container-->
 
+  <!-- Parceiros em Destaque -->
+
   <section class="parceiro-destaque-container">
 
-    <div class="bg-degrade-red">
-      <div class="container parceiro-destaque-a">
-        <div class="parceiro-destaque-info">
-          <h3>Omie Academy</h3>
-          <p>Empresa com mais de 20 anos de experiência no registro de Marcas, Patentes, Propriedade Industrial e Intelectual, Direito Autoral, Código de Barras, Softwares, Desenho Industrial, Nacional e Internacional; como também, por sua credibilidade no mercado e por disponibilizar maior leque de registros aos nossos clientes.</p>
-        </div>
-  
-        <div class="parceiro-destaque-img">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/parceiros/omnie-academy-destaque.png" alt="Omie Academy">
-        </div>
-      </div>
-    </div>
+    <?php if(have_rows('item_parceiros_destaque')) : while(have_rows('item_parceiros_destaque')) : the_row() ?>
 
-    <div class="bg-degrade-gray">
-      <div class="container parceiro-destaque-b">
-        <div class="parceiro-destaque-info">
-          <h3>Ideal Marcas e Patentes</h3>
-          <p>Empresa com mais de 20 anos de experiência no registro de Marcas, Patentes, Propriedade Industrial e Intelectual, Direito Autoral, Código de Barras, Softwares, Desenho Industrial, Nacional e Internacional; como também, por sua credibilidade no mercado e por disponibilizar maior leque de registros aos nossos clientes.</p>
-        </div>
-  
-        <div class="parceiro-destaque-img">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/parceiros/ideal-marcas-destaque.png" alt="Ideal Marcas e Patentes">
-        </div>
-      </div>
-    </div>
-
-    <div class="bg-degrade-red">
-      <div class="container parceiro-destaque-a">
-        <div class="parceiro-destaque-info">
-          <h3>Abertura Simples</h3>
-          <p>Por estarmos dispostos a atendermos todos os empreendedores, independentemente de porte, desde a abertura da empresa até as demais etapas. Acompanhamos de perto a evolução do seu negócio dando-lhes suporte necessário; evitando com isso, que a empresa tenha vida breve.</p>
-        </div>
-  
-        <div class="parceiro-destaque-img">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/parceiros/abrir-empresa-simples-destaque.png" alt="Abertura Simples">
-        </div>
-      </div>
-    </div>
+      <div class="<?php the_sub_field('parceiros_destaque_corfundo'); ?>">
+        <div class="container <?php the_sub_field('parceiros_destaque_posicao'); ?>">
+          <div class="parceiro-destaque-info">
+            <h3><?php the_sub_field('parceiros_destaque_titulo'); ?></h3>
+            <p><?php the_sub_field('parceiros_destaque_descricao'); ?></p>
+          </div>
     
-
-    <div class="bg-degrade-gray">
-      <div class="container parceiro-destaque-b">
-        <div class="parceiro-destaque-info">
-          <h3>FENACON|CD - AR Service</h3>
-          <p>Em parceria com a FENACON|CD - AR SERVICE, realizamos as etapas necessárias para emissão do seu certificado digital. Com esse recurso os empreendedores garantem uma maior segurança e confiabilidade no tráfego das suas informações, além disso, também reduzem burocracias.</p>
-        </div>
-  
-        <div class="parceiro-destaque-img">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/parceiros/fenacon-destaque.png" alt="FENACON|CD - AR Service">
+          <div class="parceiro-destaque-img">
+            <img src="<?php the_sub_field('parceiros_destaque_imagem'); ?>" alt="<?php the_sub_field('parceiros_destaque_titulo'); ?>">
+          </div>
         </div>
       </div>
-    </div>
+
+    <?php endwhile; else : endif; ?>   
+
   </section>
 
   <!-- CTA -->
