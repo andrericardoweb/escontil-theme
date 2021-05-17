313b85c781e99ab29b4482fb8f96137e37e0d8f9
Andre Ricardo <andrericardoweb@gmail.com>
2021-05-17 09:31:57 -0300

    ajustes blog

---
+++ home.php
@@ -0,0 +1,78 @@
+<?php get_header(); ?>
+
+  <!-- BANNER -->
+  <section class="banner-page">
+    <div class="container">
+      <h2>Blog</h2>
+    </div>
+  </section>
+
+  <!--Últimas Publicações-->
+  <section class="container blog-bg">
+    <h2>Últimas Publicações</h2>
+    <div class="container blog-container">
+
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+      <div class="blog-item">
+        <img src="<?php the_field('capa_do_artigo'); ?>">
+        <h3><?php the_title(); ?></h3>
+        <?php the_excerpt(); ?>
+        
+        <a href="<?php the_permalink(); ?>">Continuar lendo <i class="fas fa-arrow-right"></i></a>
+      </div><!--blog-item-->
+
+    <?php endwhile;  ?>
+
+    </div><!--blog-container-->
+
+      <div class="paginacao">
+
+        <div class="paginacao-item">
+          <?php previous_posts_link('<i class="fas fa-arrow-left"></i> Anteriores '); ?>
+        </div>
+
+        <div class="paginacao-item">
+          <?php next_posts_link('Próximos <i class="fas fa-arrow-right"></i>'); ?>
+        </div>
+        
+      </div> <!--paginacao-->
+
+
+    <?php else : endif; ?>
+  
+
+    
+
+  </section>
+
+  
+  <!-- Missao -->
+  <?php include(TEMPLATEPATH . "/inc/missao-section.php"); ?>
+
+  <section class="rodape-sugestoes-bg">
+    <div class="rodape-sugestoes container">
+      <div class="rodape-sugestoes-info">
+        <h2>Elogios,<br>sugestões e<br>reclamações<br></h2>
+        <p>Nosso objetivo é melhorar a cada dia, por isso pedimos que nos ajudem a saber onde estamos acertando ou errando, além de enviar sugestões para nosso trabalho.</p>
+      </div><!--rodape-sugestoes-bg-info-->
+      <div class="rodape-sugestoes-form">
+        <form action="">
+          <div class="rodape-sugestoes-form-2">
+            <input type="text" name="nome" id="nome" placeholder="Nome (Opcional)">
+          </div>
+          <div class="rodape-sugestoes-form-2">
+            <input type="tel" name="fone" id="fone" placeholder="Celular (Opcional)">
+            <input type="email" name="email" id="email" placeholder="E-mail (Opcional)">
+          </div>
+          <div class="rodape-sugestoes-form-2">
+            <textarea name="msg-sugestoes" id="msg-sugestoes" placeholder="Digite aqui sua sugestão, reclamação ou elogio."></textarea> 
+          </div>
+
+          <button type="submit">Enviar Mensagem</button>
+        </form>
+      </div>
+    </div>
+  </section><!--rodape-sugetoes-bg-->
+
+  <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,12 +1,35 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <!-- BANNER -->
+  <section class="banner-page">
+    <div class="container">
+      <h2>Blog</h2>
+    </div>
+  </section>
+
+  <!--Página do artigo-->
+  <section class="container artigo-container">
+
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    
+      <div class="artigo-item">
+        <h2><?php the_title(); ?></h2>
+        
+        <div class="artigo-texto">
+        <?php the_content(); ?>
+        </div>  
+      
+      </div>
+
+    <?php endwhile; else: ?>
+      
+      <p><?php _e('Desculpe, não há publicações'); ?></p>
+      
+    <?php endif ?>
   
-<h1><?php the_title(); ?></h1>
-<?php the_content(); ?>
+  </section>
 
-<?php endwhile; else: ?>
-<p><?php _e('Desculpe, não há publicações'); ?></p>
-<?php endif ?>
+  <!-- CTA -->
+  <?php // include(TEMPLATEPATH . "/inc/missao-section.php"); ?>
 
-<?php get_footer(); ?>
+  <?php get_footer(); ?>
