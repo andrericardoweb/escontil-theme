26a56466e1b0b5150a64d28ae94c097d985e6a5f
Andre Ricardo <andrericardoweb@gmail.com>
2021-06-15 13:11:14 -0300

    Animação Título das páginas interna

--- inc/introducao.php
+++ inc/introducao.php
@@ -1,5 +1,5 @@
 <section class="banner-page">
   <div class="container">
-    <h2><?php the_title(); ?></h2>
+    <h2 data-anime="400" class="fadeInDown"><?php the_title(); ?></h2>
   </div>
 </section>
